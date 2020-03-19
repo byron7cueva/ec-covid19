f01c559642ec4657b029f7bd1bb5e543a46020c2
byron7cueva <byron7cueva@gmail.com>
2020-03-18 22:37:15 -0500

    Add constants

---
+++ ec-covid19-web/src/settings/constants.js
@@ -0,0 +1,14 @@
+export const colors = {
+  first: '#e85f99',
+  second: '#50bda1',
+  alternative: '#f18867',
+  light: '#f5f6fa',
+
+  red: '#fe346e',
+  orange: '#fbc490',
+  grayDark: '#655780'
+}
+
+export const size = {
+  textContent: '12px'
+}
