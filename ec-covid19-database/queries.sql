a31c77bb5e6d38c2d3544d6b25ec22099592f079
byron7cueva <byron7cueva@gmail.com>
2020-03-25 08:13:52 -0500

    Update queries

--- ec-covid19-database/queries.sql
+++ ec-covid19-database/queries.sql
@@ -1,7 +1,7 @@
-select placeId, placeName from Place;
+select placeId, placeName from Places;
 
 /* Date total for places */
-select p.placeId, placeCode, placeName, placeTypeId, confirmed, dead, healed, updateDate from Place p  left join ConfirmedCase c on  c.placeId = p.placeId and caseTypeId = 1;
+select p.placeId, placeCode, placeName, placeTypeId, confirmed, dead, healed, updateDate from Places p  left join ConfirmedCases c on  c.placeId = p.placeId and caseTypeId = 1;
 
 /* History Day cases */
-select confirmed, dead, healed, caseDate from ConfirmedCase c where caseTypeId = 2 and placeId = 1;
+select confirmed, dead, healed, caseDate from ConfirmedCases where caseTypeId = 2 and placeId = 1;
