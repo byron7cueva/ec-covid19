3df72ab9e51c260e915eb5e4767c0bcae8433fd3
byron7cueva <byron7cueva@gmail.com>
2020-03-25 07:48:04 -0500

    Change DDL Database to podtgresql

--- ec-covid19-database/ddlCreate.sql
+++ ec-covid19-database/ddlCreate.sql
@@ -2,40 +2,58 @@
 *	DDL Database
 */
 
+/* Secuencies */
+CREATE SEQUENCE IF NOT EXISTS Places_placeId_seq
+START 1
+MINVALUE 1;
+
+CREATE SEQUENCE IF NOT EXISTS ConfirmedCases_caseId_seq
+START 1
+MINVALUE 1;
+
 /* This table has place type information */
-CREATE TABLE IF NOT EXISTS PlaceType (
-	placeTypeId INTEGER CONSTRAINT PK_PlaceType PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS PlaceTypes (
+	placeTypeId INTEGER NOT NULL,
 	placeTypeName VARCHAR(20) NOT NULL
 );
 
 /* This table has case type */
-CREATE TABLE IF NOT EXISTS CaseType (
-	caseTypeId INTEGER CONSTRAINT PK_CasesType PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS CaseTypes (
+	caseTypeId INTEGER NOT NULL,
 	type VARCHAR(20) NOT NULL
 );
 
 /* This table has information of places*/
-CREATE TABLE IF NOT EXISTS Place (
-	placeId INTEGER CONSTRAINT PK_Place PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS Places (
+	placeId INTEGER NOT NULL DEFAULT nextval('Places_placeId_seq'),
 	placeCode VARCHAR(10) NOT NULL UNIQUE,
 	placeName VARCHAR(50) NOT NULL,
 	placeTypeId INTEGER NOT NULL,
-	parentRegion INTEGER,
-	CONSTRAINT FK_PlaceType_Place FOREIGN KEY (placeTypeId) REFERENCES PlaceType(placeTypeId),
-	CONSTRAINT FK_Place_parentRegion FOREIGN KEY (parentRegion) REFERENCES Place(placeId)
+	parentRegion INTEGER
 );
 
 /* This table has information of cases */
-CREATE TABLE IF NOT EXISTS ConfirmedCase (
-	caseId INTEGER CONSTRAINT PK_Case PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS ConfirmedCases (
+	caseId INTEGER NOT NULL DEFAULT nextval('ConfirmedCases_caseId_seq'),
 	placeId INTEGER NOT NULL,
 	caseTypeId INTEGER NOT NULL,
-	caseDate TEXT,
+	caseDate DATE,
 	confirmed INTEGER DEFAULT 0,
 	dead INTEGER DEFAULT 0,
 	healed INTEGER DEFAULT 0,
-	insertDate TEXT NOT NULL,
-	updateDate TEXT NOT NULL,
-	CONSTRAINT FK_Place_ConfirmedCase FOREIGN KEY (placeId) REFERENCES Place(placeId),
-	CONSTRAINT FK_CaseType FOREIGN KEY (caseTypeId) REFERENCES CaseType(caseTypeId)
+	insertDate TIMESTAMP NOT NULL,
+	updateDate TIMESTAMP NOT NULL
 );
+
+/* Constraints */
+ALTER TABLE PlaceTypes ADD CONSTRAINT PK_PlaceTypes PRIMARY KEY (placeTypeId);
+ALTER TABLE CaseTypes ADD CONSTRAINT PK_CasesTypes PRIMARY KEY (caseTypeId);
+ALTER TABLE Places ADD CONSTRAINT PK_Places PRIMARY KEY (placeId);
+ALTER TABLE ConfirmedCases ADD CONSTRAINT PK_ConfirmedCases PRIMARY KEY (caseId);
+
+ALTER TABLE Places ADD CONSTRAINT FK_PlaceTypes_Places FOREIGN KEY (placeTypeId) REFERENCES PlaceTypes(placeTypeId);
+ALTER TABLE Places ADD CONSTRAINT FK_Places_parentRegion FOREIGN KEY (parentRegion) REFERENCES Places(placeId);
+
+
+ALTER TABLE ConfirmedCases ADD CONSTRAINT FK_Places_ConfirmedCases FOREIGN KEY (placeId) REFERENCES Places(placeId);
+ALTER TABLE ConfirmedCases ADD CONSTRAINT FK_CaseTypes FOREIGN KEY (caseTypeId) REFERENCES CaseTypes(caseTypeId);
