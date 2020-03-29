35466637f1c370a2c1a7b6aedfd7c79beba8867c
byron7cueva <byron7cueva@gmail.com>
2020-03-28 23:17:26 -0500

    Queries

--- ec-covid19-database/queries.sql
+++ ec-covid19-database/queries.sql
@@ -1,10 +1,62 @@
-select placeId, placeName from Places;
+/* Obtener los ultimos casos por cantones */
+select p.placeCode, placeName, x, y, placeTypeId, parentRegion, caseDate, confirmed, totalConfirmed, dead, totalDead, healed, totalhealed, updateDate
+from places p
+left join confirmedcases c on p.placeCode = c.placeCode and c.caseDate = (select max(caseDate) from confirmedcases where placeCode = p.placeCode)
+where placeTypeId = 4;
 
-/* Date total for places */
-select p.placeId, placeCode, placeName, placeTypeId, confirmed, dead, healed, updateDate from Places p  left join ConfirmedCases c on  c.placeId = p.placeId and caseTypeId = 1;
+/* Obtener ultimos casos por provincias */
+select r.placeCode, r.placeName, r.placeTypeId, r.parentRegion, sum(confirmed) as confirmed, sum(totalConfirmed) as totalConfirmed, sum(dead) as dead, sum(totalDead) as totalDead, sum(healed) as healed, sum (totalhealed) as totalhealed
+from places r
+left join places p on p.parentRegion = r.placeCode
+left join confirmedcases c on p.placeCode = c.placeCode and c.caseDate = (select max(caseDate) from confirmedcases where placeCode = p.placeCode)
+where p.placeTypeId = 4
+group by r.placeCode;
 
-/* History Day cases */
-select confirmed, dead, healed, caseDate from ConfirmedCases where caseTypeId = 2 and placeId = 1;
+/* Casos por Region */
+select g.placeCode, g.placeName, g.placeTypeId, g.parentRegion, sum(confirmed) as confirmed, sum(totalConfirmed) as totalConfirmed, sum(dead) as dead, sum(totalDead) as totalDead, sum(healed) as healed, sum (totalhealed) as totalhealed
+from places g
+left join places r on r.parentRegion = g.placeCode
+left join places p on p.parentRegion = r.placeCode
+left join confirmedcases c on p.placeCode = c.placeCode and c.caseDate = (select max(caseDate) from confirmedcases where placeCode = p.placeCode)
+where p.placeTypeId = 4
+group by g.placeCode;
 
-/* Get last cases */
-select p.placeId, placeCode, placeName, x, y, placeTypeId, parentRegion, caseDate, confirmed, dead, healed, updateDate from places p left join confirmedcases c on c.caseId IS NULL OR (p.placeId = c.placeId and c.caseDate = (select max(caseDate) from confirmedcases where placeId = p.placeId));
+/* Total casos Ecuador */
+select sum(confirmed) as confirmed, sum(totalConfirmed) as totalConfirmed, sum(dead) as dead, sum(totalDead) as totalDead, sum(healed) as healed, sum (totalhealed) as totalhealed
+from places p
+inner join confirmedcases c on p.placeCode = c.placeCode and c.caseDate = (select max(caseDate) from confirmedcases where placeCode = p.placeCode)
+where placeTypeId = 4;
+
+
+/* Obtener historial de casos de un canton */
+select placeCode, caseDate, confirmed, totalConfirmed, dead, totalDead, healed, totalhealed, updateDate
+from confirmedcases
+where placeCode = '0901'
+order by caseDate ASC;
+
+/* Obtener historial de casos por provincias */
+select r.placeCode, r.placeName, r.placeTypeId, r.parentRegion, sum(confirmed) as confirmed, sum(totalConfirmed) as totalConfirmed, sum(dead) as dead, sum(totalDead) as totalDead, sum(healed) as healed, sum (totalhealed) as totalhealed
+from places r
+inner join places p on p.parentRegion = r.placeCode
+inner join confirmedcases c on p.placeCode = c.placeCode
+where p.placeTypeId = 4 and r.placeCode = '09'
+group by r.placeCode, caseDate
+order by caseDate ASC;
+
+/* Historial de casos por region */
+select g.placeCode, g.placeName, g.placeTypeId, g.parentRegion, sum(confirmed) as confirmed, sum(totalConfirmed) as totalConfirmed, sum(dead) as dead, sum(totalDead) as totalDead, sum(healed) as healed, sum (totalhealed) as totalhealed
+from places g
+inner join places r on r.parentRegion = g.placeCode
+inner join places p on p.parentRegion = r.placeCode
+inner join confirmedcases c on p.placeCode = c.placeCode
+where p.placeTypeId = 4 and r.placeCode = '1'
+group by g.placeCode, caseDate
+order by caseDate ASC;
+
+/* Historial casos Ecuador */
+select sum(confirmed) as confirmed, sum(totalConfirmed) as totalConfirmed, sum(dead) as dead, sum(totalDead) as totalDead, sum(healed) as healed, sum (totalhealed) as totalhealed
+from places p
+inner join confirmedcases c on p.placeCode = c.placeCode
+where placeTypeId = 4
+group by caseDate
+order by caseDate ASC;
