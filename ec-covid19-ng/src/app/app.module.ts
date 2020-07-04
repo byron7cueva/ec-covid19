c438c9ef1d4f7c7ebd78b8ebd211934e973a2fb0
byron7cueva <byron7cueva@gmail.com>
2020-07-04 15:42:50 -0500

    Configure scss structure and add header and footer

--- ec-covid19-ng/src/app/app.module.ts
+++ ec-covid19-ng/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 
@@ -12,7 +14,8 @@ import { LayoutComponent } from './layout/layout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
