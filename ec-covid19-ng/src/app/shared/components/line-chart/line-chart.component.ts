bd1bda8b62e7f05096cc2e8767301779b02b0a16
byron7cueva <byron7cueva@gmail.com>
2020-07-07 22:28:10 -0500

    LineChart con soporte de varias lineas

--- ec-covid19-ng/src/app/shared/components/line-chart/line-chart.component.ts
+++ ec-covid19-ng/src/app/shared/components/line-chart/line-chart.component.ts
@@ -9,6 +9,9 @@ import * as d3Array from 'd3-array';
 
 import { Margin } from '@core/interfaces/Margin';
 
+const parseTime = d3TimeFormat.timeParse('%Y-%m-%d');
+const xFormat = '%-m/%-d';
+
 @Component({
   selector: 'app-line-chart',
   templateUrl: './line-chart.component.html',
@@ -45,11 +48,14 @@ export class LineChartComponent implements OnInit, AfterViewInit {
     this.buildSvg();
     this.buildAxis();
     this.drawLine();
+    this.drawLegend();
   }
 
+  /**
+   * Add svg to component
+   */
   private buildSvg(): void {
     if (this.widthSvg && this.heightSvg) {
-      // Add svg
       this.svg = d3Selection.select(document.getElementById(this.id))
         .attr('width', this.width)
         .attr('height', this.height)
@@ -58,56 +64,90 @@ export class LineChartComponent implements OnInit, AfterViewInit {
     }
   }
 
-  private buildAxis(): void {
-    if (this.data) {
-      const parseTime = d3TimeFormat.timeParse('%Y-%m-%d');
-      const xFormat = '%-m/%-d';
-      const dataXrange = d3Array.extent(this.data, d => parseTime(d.x));
-      const dataYrange = [0, d3Array.max(this.data, d => d.y)];
-      this.xScale = d3Scale.scaleTime()
-      // .domain([new Date('2020-03-13'), new Date('2020-03-23')])
+  private buildAxis() {
+    const dataXY = this.data.reduce((acc, cur, i) => {
+      if (i === 1) {
+        return acc.data.concat(cur.data);
+      }
+      return acc.concat(cur.data);
+    });
+
+    const dataXrange = d3Array.extent(dataXY, d => parseTime(d.x));
+    const dataYrange = [0, d3Array.max(dataXY, d => d.y)];
+
+    this.xScale = d3Scale.scaleTime()
         .domain(dataXrange)
         .range([0, this.widthSvg]);
 
-      // x axis in group tag
-      this.svg.append('g')
-        .attr('class', 'x axis')
-        .attr('transform', `translate(0, ${this.heightSvg})`)
-        .call(d3Axis.axisBottom(this.xScale).tickFormat(d3TimeFormat.timeFormat(xFormat)));
-
-      this.yScale = d3Scale.scaleLinear()
-        // .domain([0, 981])
+    this.yScale = d3Scale.scaleLinear()
         .domain(dataYrange)
         .range([this.heightSvg, 0]);
 
-        // y axis inn group tag
-      this.svg.append('g')
-        .attr('class', 'y axis')
-        .call(d3Axis.axisLeft(this.yScale));
-      }
+    // x axis in group tag
+    this.svg.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', `translate(0, ${this.heightSvg})`)
+    .attr('color', '#f5f6fa')
+    .call(d3Axis.axisBottom(this.xScale).tickFormat(d3TimeFormat.timeFormat(xFormat)));
+
+    // y axis inn group tag
+    this.svg.append('g')
+      .attr('class', 'y axis')
+      .attr('color', '#f5f6fa')
+      .call(d3Axis.axisLeft(this.yScale));
   }
 
   private drawLine() {
-    const parseTime = d3TimeFormat.timeParse('%Y-%m-%d');
     const line = d3Shape.line()
     .x(d => this.xScale(parseTime(d.x)))
     .y(d => this.yScale(d.y))
     .curve(d3Shape.curveMonotoneX);
 
-    this.svg.append('path')
-      .datum(this.data)
-      .attr('class', 'line')
-      .attr('d', line)
-      .attr('fill', 'none')
-      .attr('stroke', '#ffab00');
-
-    this.svg.selectAll('.dot')
+    const lines = this.svg.selectAll('lines')
       .data(this.data)
+      .enter()
+      .append('g');
+
+    lines.append('path')
+    .attr('class', d => d.id)
+    .attr('d', d => line(d.data))
+    .attr('fill', 'none')
+    .attr('stroke', d => d.color);
+
+    this.data.forEach(item => {
+      this.svg.selectAll(`.${item.id}`)
+      .data(item.data)
       .enter()
       .append('circle')
       .attr('class', 'dot')
       .attr('cx', d => this.xScale(parseTime(d.x)))
       .attr('cy', d => this.yScale(d.y))
-      .attr('r', 5);
+      .attr('r', 3)
+      .attr('fill', item.color);
+    });
+  }
+
+  private drawLegend() {
+    const numSpaces = this.data.length + 2;
+    const widthSpace = this.width / numSpaces;
+    const widthLegend = 70;
+    const widthRect = 5;
+    const legend = this.svg.selectAll('legend')
+      .data(this.data)
+      .enter()
+      .append('g')
+      .attr('class', 'legend');
+
+    legend.append('circle')
+    .attr('cy', this.height - widthLegend)
+    .attr('cx', (d, i) => widthSpace * (i + 1))
+    .attr('r', widthRect)
+    .style('fill', d => d.color);
+
+    legend.append('text')
+      .attr('y', this.height - widthLegend + widthRect)
+      .attr('x', (d, i) => widthSpace * (i + 1) + 12)
+      .text(d => d.id)
+      .attr('fill', d => d.color);
   }
 }
